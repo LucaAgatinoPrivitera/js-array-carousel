2a014ad0099e56b13124187c11832282ed612048
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2024-04-23 16:41:47 +0200

    M4 - Sto impazzendo cercando di capire perché javascript è così stupido

--- script/main.js
+++ script/main.js
@@ -4,7 +4,7 @@ let prevSlide;
 
 let slides = document.getElementsByClassName("slide");
 
-document.getElementById("btnNext").addEventListener("click", function nextImage () {
+document.getElementById("btnNext").addEventListener("click", function nextImage() {
     for (let i = 0; i < slides.length; i++) {
 
         const slide = slides[i]; //non ho ben capito perché qui prenda tutti gli i, invece di solamente l'ultimo. Ricollego il dubbio alla riga 17
@@ -33,7 +33,7 @@ document.getElementById("btnNext").addEventListener("click", function nextImage 
     console.log(`Sposto la selezione da ${immagineAttualmenteAttiva} a ${prossimaSlide}`);
 })
 
-document.getElementById("btnPrev").addEventListener("click", function prevImage () {
+document.getElementById("btnPrev").addEventListener("click", function prevImage() {
     for (let i = 0; i < slides.length; i++) {
 
         const slide = slides[i];
@@ -47,12 +47,13 @@ document.getElementById("btnPrev").addEventListener("click", function prevImage 
         }
     }
 
-    //prossima slide sarà idealmente quella attuale + 1
+    //precedente slide sarà idealmente quella attuale - 1
     prevSlide = immagineAttualmenteAttiva - 1;
 
     //se però l'immagine successiva non c'è, allora resetta la slide alla prima
-    if (prevSlide >= slides.length) {
-        prevSlide = 0;
+    if (prevSlide = slides[0]) {
+        //aiuto visivo per me, non è un merge
+        prevSlide = slides.length;
     }
 
     //in ogni caso, rendi active la slide che ci interessa
@@ -60,43 +61,3 @@ document.getElementById("btnPrev").addEventListener("click", function prevImage 
 
     console.log(`Sposto la selezione da ${immagineAttualmenteAttiva} a ${prevSlide}`);
 })
-
-
-
-// document.getElementById("btnNext").addEventListener("click", function () {
-//     // iterare su tutte le slide
-//     for (let i = 0; i < slides.length; i++) {
-
-//         const slide = slides[i];
-//         // console.log(i, slide);
-
-//         //controlliamo se questa slide ha la classe active
-//         if (slide.classList.contains("active")) {
-//             //se si, rimuovo la classe active
-//             slide.classList.remove("active");
-//             //e prendo nota dell'indice
-//             immagineAttualmenteAttiva = i;
-//             // console.log("Ho rimosso la classe active dall'elemento: ", i);
-//         }
-//     }
-
-//     //prossima slide sarà idealmente quella attuale + 1
-//     prossimaSlide = immagineAttualmenteAttiva + 1;
-
-//     //se però l'immagine successiva non c'è, allora resetta la slide alla prima
-//     if (prossimaSlide >= slides.length) {
-//         prossimaSlide = 0;
-//     }
-
-//     //in ogni caso, rendi active la slide che ci interessa
-//     slides[prossimaSlide].classList.add("active");
-
-//     console.log(`Sposto la selezione da ${immagineAttualmenteAttiva} a ${prossimaSlide}`);
-// });
-
-
-// //Esempio di event handler
-// document.getElementById("btnPrev").addEventListener("click", function () {
-//     //CODICE DA ESEGUIRE AL CLICK VA QUI
-//     console.log("Sono un pulsante ma non ho uno scopo, aiutami!");
-// });
